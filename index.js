b8607d7fc72c28c3a413163b1c4278358139a28c
Erick C <cherick04@hotmail.com>
2022-02-08 11:41:44 -0300

    Implemented basic functionality

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const allCharacters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*"
+const copyMsg = "Click on any of the passwords to copy it to your clipboard"
 
-const passButton1 = document.getElementById("pass1-btn")
-const passButton2 = document.getElementById("pass2-btn")
-const passButton3 = document.getElementById("pass3-btn")
-const passButton4 = document.getElementById("pass4-btn")
+const passLengthSelector = document.getElementById("length-sel")
+const copyLbl = document.getElementById("copy-lbl")
+const passTxt1 = document.getElementById("pass1-txt")
+const passTxt2 = document.getElementById("pass2-txt")
+const passTxt3 = document.getElementById("pass3-txt")
+const passTxt4 = document.getElementById("pass4-txt")
 
-let buttons = [passButton1, passButton2, passButton3, passButton4]
+let txtBoxes = [passTxt1, passTxt2, passTxt3, passTxt4]
 
-
-function generatePasswords(maxLength = 16) {
-    for(let btnIndex=0; btnIndex<4; btnIndex++) {
+function generatePasswords() {
+    const maxLength = Number(passLengthSelector.value)
+    for(let j=0; j<4; j++) {
         let randomPassword = ''
         for(let i=0; i < maxLength; i++) {
             let randomIndex = Math.floor(Math.random() * allCharacters.length)
             randomPassword += allCharacters[randomIndex]
         }
-        buttons[btnIndex].style.backgroundImage = 'none'
+        txtBoxes[j].style.backgroundImage = 'none'
 
-        buttons[btnIndex].textContent = randomPassword
+        txtBoxes[j].value = randomPassword
     }
+    copyLbl.textContent = copyMsg
 }
 
-function copyPassword(buttonNumber) {
-
+function copyPassword(id) {
+    copyLbl.textContent = ''
+    const password = document.getElementById(id)
+    password.select()
+    document.execCommand('copy')
+    copyLbl.textContent = 'Copied!'
 }
